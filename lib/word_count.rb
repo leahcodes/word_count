414b7fa590c47db013e98ef2c0021b59d9880612
Leah Klamkin <leahcodes@gmail.com>
2015-08-07 14:34:38 -0700

    Styled page.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -10,29 +10,6 @@ class String
     else
       0
     end
+
   end
 end
-
-      # initial_array.each() do |initial_word|
-      #   until initial_array.at(-1) do
-      #     initial_word == second_word
-      #   end
-      #   returns count
-      # end
-
-
-
-    # .join().split("")
-
-    # special_char_array = [".", ",", "!", "-", "&", ":", "''",]
-    # bad_char_array = []
-    # no_special_char_array = []
-
-    # special_char_array.each() do |special|
-    #   if initial_array.eql?(special)
-    #     bad_char_array.push(initial_array)
-    #   else
-    #     no_special_char_array.push(initial_array)
-    #   end
-    #   no_special_char_array
-    # end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -11,14 +11,14 @@ describe('String#word_count') do
 #   end
 
   it ('loops through the array to see if the argument is included in the array') do
-    expect("Dog eat dog.".word_count("cat")).to(eq("Word not found!"))
+    expect("Dog eat dog.".word_count("cat")).to(eq(0))
   end
 
   it ('loops through the array and tells how many times the word is included in the array') do
     expect("Dog eat dog eats doggy eats dog eats dog".word_count("dog")).to(eq(4))
   end
-  # it ('handles special characters') do
-  #   expect(("Dog eat dog!!").word_count).to(eq("dog eat dog"))
-  # end
 
+    it ('handles special characters') do
+      expect("Dog eats dog!".word_count("dog")).to(eq(2))
+  end
 end
