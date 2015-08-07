4efcbbb0c5a809b910699dad5ec0bd4db8d5e3db
Leah Klamkin <leahcodes@gmail.com>
2015-08-07 09:33:42 -0700

    It handles capitals.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
+  it ('handles capitals') do
+    expect(("Dog eat dog.").word_count).to(eq("dog eat dog."))
+  end
+
   it ('creates an array of the string') do
-    expect(("Dog eat dog.").word_count).to(eq(["Dog", "eat", "dog."]))
+    expect(("Dog eat dog.").word_count).to(eq(["dog", "eat", "dog."]))
   end
 end
